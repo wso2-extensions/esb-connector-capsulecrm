d9cc51fb5699128341223129d55c1bdf8484300d
keetz <heykeetz@gmail.com>
2016-07-14 16:18:59 +0530

    After the release

--- src/test/java/org/wso2/carbon/connector/integration/test/capsulecrm/CapsuleCRMConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/capsulecrm/CapsuleCRMConnectorIntegrationTest.java
@@ -46,7 +46,7 @@ public class CapsuleCRMConnectorIntegrationTest extends ConnectorIntegrationTest
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("capsulecrm-connector-1.0.2");
+        init("capsulecrm-connector-1.0.3-SNAPSHOT");
 
         esbRequestHeadersMap = new HashMap<String, String>();
         apiRequestHeadersMap = new HashMap<String, String>();
